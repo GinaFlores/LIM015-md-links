64e906f32b808b255695147221d6f34075b1bea8
Gina <floresgina2020@gmail.com>
2021-09-24 00:25:30 -0500

    nuevos archivos, funciones pequeñas terminadas y test

--- src/index.js
+++ src/index.js
@@ -1,19 +1,42 @@
-const ruta = 'C:\\Users\\Laboratoria\\OneDrive\\Documentos\\Laboratoria015\\LIM015-md-links\\README.md' // Ruta absoluta
-const otraRuta = 'README.md' // Ruta relativa
-
 const path = require('path');
 const fs = require('fs');
 
+const ruta = 'C:\\Users\\Laboratoria\\OneDrive\\Documentos\\Laboratoria015\\LIM015-md-links\\README.md' // Ruta absoluta
+const otraRuta = 'README.md' // Ruta relativa
+
 // Validar si existe la ruta
 const existPath = (route) => fs.existsSync(route); // Retorna booleano
-console.log(existPath(ruta));
 //const route = process.argv[2];
 //console.log(route);
 
 // Verificar si es absoluta. Y si no lo es, convertirla a absoluta
 const absolutePath = (route) => path.isAbsolute(route) ? (route) : path.resolve(route); // Retorna ruta absoluta
-console.log(absolutePath(otraRuta));
 
 // Verificar si es un directorio
 const isDirectory = (route) => fs.statSync(route).isDirectory(); // Retorna booleano
-console.log('¿Es un directorio?', isDirectory('prueba'));
+
+// Verificar si es un archivo
+const isFile = (route) => fs.statSync(route).isFile(); // Retorna booleano
+
+// Uniendo dos rutas
+const joinPaths = (route, route2) => path.join(route, route2);
+
+// Leer un archivo
+const readFile = (route) => fs.readdirSync(route); // Retorna array de archivos o carpetas que hay en el directorio
+
+// Leer extension md
+const isFileMd = (route) => (path.extname(route) === '.md'); // Retorna la extensión de la ruta
+
+// Lee contenido que hay dentro de un archivo
+const contentFile = (route) => fs.readFileSync(route, 'utf-8');
+
+module.exports = {
+    existPath,
+    absolutePath,
+    isDirectory,
+    isFile,
+    joinPaths,
+    readFile,
+    isFileMd,
+    contentFile,
+}
--- test/md-links.spec.js
+++ test/md-links.spec.js
@@ -1,9 +1,100 @@
-const mdLinks = require('../');
+// const mdLinks = require('../');
+const {
+  existPath,
+  absolutePath,
+  isDirectory,
+  isFile,
+  joinPaths,
+  readFile,
+  isFileMd,
+  contentFile,
+} = require('../src/index')
 
-describe('mdLinks', () => {
 
-  it('should...', () => {
-    console.log('FIX ME!');
+describe('existPath', () => {
+  it('debería ser una función', () => {
+    expect(typeof(existPath)).toBe('function');
   });
+  it('deberia retornar true si la ruta existe', () => {
+    expect(existPath('prueba')).toBe(true);
+  });
+  it('deberia retornar false si la ruta no existe', () => {
+    expect(existPath('gitignore')).toBe(false);
+  });
+});
 
+describe('absolutePath', () => {
+  it('debería ser una función', () => {
+    expect(typeof(absolutePath)).toBe('function');
+  });
+  it('deberia retornar convertir la ruta absoluta', () => {
+    expect(absolutePath('README.md')).toBe('C:\\Users\\Laboratoria\\OneDrive\\Documentos\\Laboratoria015\\LIM015-md-links\\README.md');
+  });
+  it('deberia retornar la ruta absoluta', () => {
+    expect(absolutePath('C:\\Users\\Laboratoria\\OneDrive\\Documentos\\Laboratoria015\\LIM015-md-links\\README.md')).toBe('C:\\Users\\Laboratoria\\OneDrive\\Documentos\\Laboratoria015\\LIM015-md-links\\README.md');
+  });
+});
+
+describe('isDirectory', () => {
+  it('debería ser una función', () => {
+    expect(typeof(isDirectory)).toBe('function');
+  });
+  it('deberia retornar true si es un directorio', () => {
+    expect(isDirectory('prueba')).toBe(true);
+  });
+  it('deberia retornar false si no es un directorio', () => {
+    expect(isDirectory('README.md')).toBe(false);
+  });
+});
+
+describe('isFile', () => {
+  it('debería ser una función', () => {
+    expect(typeof(isFile)).toBe('function');
+  });
+  it('debería retornar true si es un archivo', () => {
+    expect(isFile('README.md')).toBe(true);
+  });
+  it('deberia retornar false si no es un archivo', () => {
+    expect(isFile('prueba')).toBe(false);
+  });
+});
+
+describe('joinPaths', () => {
+  it('debería ser una función', () => {
+    expect(typeof(joinPaths)).toBe('function');
+  });
+  it('debería retornar dos rutas unidas', () => {
+    const result = '\\home\\Laboratoria\\test';
+    expect(joinPaths('/home/Laboratoria/', './test')).toBe(result);
+  });
+});
+
+describe('readFile', () => {
+  it('debería ser una función', () => {
+    expect(typeof(readFile)).toBe('function');
+  });
+  it('debería ser un array con los archivos del directorio', () => {
+    expect(readFile('prueba')).toEqual([ 'cursos', 'pruebaRecetas', 'README1.md' ]);
+  });
+});
+
+describe('isFileMd', () => {
+  it('debería ser una función', () => {
+    expect(typeof(isFileMd)).toBe('function');
+  });
+  it('deberia retornar true para la extension .md', () => {
+    expect(isFileMd('README.md')).toBe(true);
+  });
+  it('deberia retornar false para la extension .js', () => {
+    expect(isFileMd('index.js')).toBe(false);
+  });
+});
+
+describe('contentFile', () => {
+  it('debería ser una función', () => {
+    expect(typeof(contentFile)).toBe('function');
+  });
+  it('deberia retornar el contenido del archivo', () => {
+    expect(contentFile('prueba\\cursos\\README2.md')).toEqual(`[Markdown](https://es.wikipedia.org/wiki/Markdown)`);
+  });
 });
