a69a8902ff46e369a9c50c32537673b3630f115a
Gina <floresgina2020@gmail.com>
2021-10-07 22:51:59 -0500

    primer avance deÃl cli, implementacion del bin e instalacion del chalk

---
+++ src/cli.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+const { mdLinks } = require('../src/mdLinks.js');
+const { statsLinks, brokenLinks, help, errorRoute, notExist } = require('../src/cli-stats.js');
+const chalk = require('chalk');
+// console.log(chalk.red('hola mundo'));
+
+
