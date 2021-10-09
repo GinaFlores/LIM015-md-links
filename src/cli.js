bc30210bfbd2687ec089d58a417e30eebac35094
Gina <floresgina2020@gmail.com>
2021-10-08 23:34:47 -0500

    funciion cli terminada

--- src/cli.js
+++ src/cli.js
@@ -2,6 +2,43 @@
 const { mdLinks } = require('../src/mdLinks.js');
 const { statsLinks, brokenLinks, help, errorRoute, notExist } = require('../src/cli-stats.js');
 const chalk = require('chalk');
-// console.log(chalk.red('hola mundo'));
 
+// console.log(process.argv);
+const options = process.argv.slice(2);
+const userPath = process.argv[2]
 
+const validate = options.includes('--validate');
+const stats = options.includes('--stats');
+
+if (options.length === 1) {
+    mdLinks(userPath, {validate:false})
+    .then(res => console.log(res))
+    .catch((rej) => {
+        if (rej === 'La ruta no existe') {
+            console.log(chalk.magentaBright(notExist));
+        } else {
+            console.log(chalk.cyanBright(errorRoute));
+        }
+    })
+} else {
+    if (validate && stats) {
+        mdLinks(userPath, {validate:true})
+        .then(res => {
+            console.table(chalk.greenBright(statsLinks(res)))
+            console.table(chalk.redBright(brokenLinks(res)))
+        })
+        .catch(() => console.log(chalk.cyanBright(errorRoute)))
+    } else if (validate) {
+        mdLinks(userPath, {validate:true})
+        .then(res => console.log(res))
+        .catch(()=> console.log(chalk.cyanBright(errorRoute)))
+    } else if (stats) {
+        mdLinks(userPath, {validate:true})
+        .then(res => console.table(chalk.greenBright(statsLinks(res))))
+        .catch(()=> console.log(chalk.cyanBright(errorRoute)))
+    } else {
+        mdLinks(userPath, {validate:true})
+        .then( console.log(chalk.cyanBright(help)))
+        .catch(()=>console.log(chalk.cyanBright(errorRoute)))
+    }
+}
